e66ea6d497c4709bf533f30ce535659351736eb7
Steve Eirium <drsteve@cpan.org>
2011-11-11 22:44:45 +0000

    Import of DRSTEVE/Crypt-Password-0.06 from CPAN.
    gitpan-cpan-distribution: Crypt-Password
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         DRSTEVE/Crypt-Password-0.06.tar.gz
    gitpan-cpan-author:       DRSTEVE
    gitpan-cpan-maturity:     released

--- lib/Crypt/Password.pm
+++ lib/Crypt/Password.pm
@@ -1,7 +1,7 @@
 package Crypt::Password;
 use Exporter 'import';
-@EXPORT = ('password');
-our $VERSION = "0.05";
+@EXPORT = ('password', 'crypt_password');
+our $VERSION = "0.06";
 
 use Carp;
 
@@ -11,22 +11,32 @@ use overload
     'nomethod' => \&crypt;
 
 # from libc6 crypt/crypt-entry.c
-our %alg_to_magic = (
-    md5 => '$1$',
-    sha256 => '$5$',
-    sha512 => '$6$',
+our %alg_to_id = (
+    md5 => '1',
+    blowfish => '2a',
+    sha256 => '5',
+    sha512 => '6',
 );
+our %id_to_alg = reverse %alg_to_id;
+sub _default_algorithm { "sha256" }
 
-our %magic_to_alg = reverse %alg_to_magic;
+our $glib = (`man crypt`)[-1] !~ /FreeSec/;
+
+our $definitely_crypt;
 
 sub new {
     shift;
     password(@_);
 }
 
+sub crypt_password {
+    local $definitely_crypt = 1;
+    return password(@_);
+}
+
 sub password {
     my $self = bless {}, __PACKAGE__;
-    
+
     $self->input(shift);
     
     unless ($self->{crypted}) {
@@ -46,26 +56,60 @@ sub crypt {
     $self->{crypted} ||= $self->_crypt
 }
 
-
-
 sub input {
     my $self = shift;
     $self->{input} = shift;
-    if ($self->_looks_crypted($self->{input})) {
+    if (!$definitely_crypt && $self->_looks_crypted($self->{input})) {
         $self->{crypted} = delete $self->{input}
     }
+}
+
+sub _looks_crypted {
+    my $self = shift;
+    my $string = shift || return;
+    $glib ? $string =~ m{^\$.+\$.*\$.+$}
+            # with our dollar-signs added in around the salt
+          : $string =~ /^\$(_.{8}|.{2})\$ (.{11})?$/x
 }
 
 sub salt {
     my $self = shift;
     my $provided = shift;
     if (defined $provided) {
-        $self->{salt} = $provided
+        if (!$glib) {
+            # salt must be 2 or 8 or entropy leaks in around the side
+            # I am serious
+            if ($provided =~    m/^\$(_.{8}|_?.{2})\$(.{11})?$/
+                || $provided =~ m/^  (_.{8}|_?.{2})  (.{11})?$/x) {
+                $provided = $1;
+            }
+            if ($provided =~ /^_..?$/) {
+                croak "Bad salt input:"
+                    ." 2-character salt cannot start with _";
+            }
+            $provided =~ s/^_//;
+            if ($provided !~ m/^(.{8}|.{2})$/) {
+                croak "Bad salt input:"
+                    ." salt must be 2 or 8 characters long";
+            }
+        }
+        $self->{salt} = $provided;
     }
     else {
-        $self->{salt} ||= do {
+        return $self->{salt} if defined $self->{salt};
+        return $self->{salt} = do {
             if ($self->{crypted}) {
-                (split /\$/, $self->{crypted})[2]
+                if ($glib) {
+                    (split /\$/, $self->{crypted})[2]
+                }
+                else {
+                    $self->{crypted} =~ /^\$(_.{8}|.{2})\$ (.{11})?$/x;
+                    my $s = $1;
+                    $s || croak "Bad crypted input:"
+                            ." salt must be 2 or 8 characters long";
+                    $s =~ s/^_//;
+                    $s
+                }
             }
             else {
                 $self->_invent_salt()
@@ -78,13 +122,15 @@ sub algorithm {
     my $self = shift;
     $alg = shift;
     if ($alg) {
-        if (exists $alg_to_magic{lc $alg}) {
-            $self->{algorithm_magic} = $alg_to_magic{lc $alg};
+        $alg =~ s/^\$(.+)\$$/$1/;
+        if (exists $alg_to_id{lc $alg}) {
+            $self->{algorithm_id} = $alg_to_id{lc $alg};
             $self->{algorithm} = lc $alg;
         }
         else {
-            $self->{algorithm_magic} = $alg;
-            $self->{algorithm} = $magic_to_alg{lc $alg};
+            # $alg will be passed anyway, it may not be known to %id_to_alg
+            $self->{algorithm_id} = $alg;
+            $self->{algorithm} = $id_to_alg{lc $alg};
         }
     }
     elsif (!$self->{algorithm}) {
@@ -99,35 +145,65 @@ sub _crypt {
     my $self = shift;
     
     defined $self->{input} || croak "no input!";
-    $self->{algorithm_magic} || croak "no algorithm!";
+    $self->{algorithm_id} || croak "no algorithm!";
     defined $self->{salt} || croak "invalid salt!";
-    
-    CORE::crypt(delete $self->{input}, $self->{algorithm_magic}.$self->{salt})
+
+    my $input = delete $self->{input};
+    my $salt = $self->_form_salt();
+
+    return _do_crypt($input, $salt);
 }
 
 sub check {
     my $self = shift;
     my $plaintext = shift;
-    
-    CORE::crypt($plaintext, $self) eq "$self";
+   
+    my $salt = $self->_form_salt();
+    my $new = _do_crypt($plaintext, $salt);
+    return $new eq "$self";
 }
 
+sub _do_crypt {
+    my ($input, $salt) = @_;
+    my $crypt = CORE::crypt($input, $salt);
+    if (!$glib) {
+        # FreeSec
+        # makes pretty ambiguous crypt strings, lets add some dollar signs
+        $crypt =~ s/^(_.{8}|..)(.{11})$/\$$1\$$2/
+            || croak "failed to understand FreeSec crypt: '$crypt'";
+    }
+    return $crypt;
+}
 
+sub _form_salt {
+    my $self = shift;
+    my $s = $self->salt;
+    croak "undef salt!?" unless defined $s;
+    if ($glib) {
+        # glib
+        if ($self->{algorithm_id}) {
+            $s = sprintf('$%s$%s', $self->{algorithm_id}, $s);
+        }
+        else {
+            # ->check(), alg and salt from ourselves
+            $s = "$self";
+        }
+    }
+    else {
+        # FreeSec
+        if (length($s) == 8) {
+            $s = "_$s"
+        }
+        return $s;
+    }
+    return $s;
+}
 
-our @valid_salt = ( "a".."z", "A".."Z", "0".."9", qw(/ \ ! @ % ^), "#" );
+our @valid_salt = ( "/", ".", "a".."z", "A".."Z", "0".."9" );
 
 sub _invent_salt {
-    join "", map { $valid_salt[rand(@valid_salt)] } 1..8;
-}
-
-sub _looks_crypted {
-    my $self = shift;
-    my $string = shift;
-    $string && $string =~ m{^\$\d+\$.*\$.+$}
-}
-
-sub _default_algorithm {
-    "sha256"
+    my $many = $_[1] || 8;
+    join "", map { $valid_salt[rand(@valid_salt)] } 1..$many;
 }
 
 1;
@@ -142,20 +218,26 @@ Crypt::Password - Unix-style, Variously Hashed Passwords
 
  use Crypt::Password;
  
- my $hashed = password("password");
+ my $hashed = password("newpassword");
  
  $user->set_password($hashed);
  
- if ($user->get_password eq password($from_client)) {
+ if (password($from_database)->check($password_from_user)) {
      # authenticated
  }
- 
- # This is called Modular Crypt Format.
- 
- if (password($from_database)->check($from_user)) {
-     # authenticated
- }
- 
+
+ my $definitely_crypted_just_then = crypt_password($maybe_already_crypted);
+
+ # you also might want to
+ password($a) eq password($b)
+ # WARNING: password() will embody but not crypt an already crypted string
+ #          if you are checking something from the outside world, use check()
+
+ # imagine stealing a crypted string and using it as a password. it happens.
+
+ # WARNING: the following applies to glibc's crypt() only
+ #          Non-Linux systems beware.
+
  # Default algorithm, supplied salt:
  my $hashed = password("password", "salt");
  
@@ -169,9 +251,8 @@ Crypt::Password - Unix-style, Variously Hashed Passwords
 
 This is just a wrapper for perl's C<crypt()>, which can do everything you would
 probably want to do to store a password, but this is to make usage easier.
-
 The object stringifies to the return string of the crypt() function, which is
-usually (see L<KNOWN ISSUES>) in Modular Crypt Format:
+(B<on Linux/glibc>) in Modular Crypt Format:
 
  # scalar($hashed):
  #    v digest   v hash ->
@@ -179,11 +260,24 @@ usually (see L<KNOWN ISSUES>) in Modular Crypt Format:
  #      ^ salt ^
 
 That you can store, etc, retrieve then give it to C<password()> again to
-C<-E<gt>check($given_password)> or string compare to the output of a new
-C<password($given_password)>.
+C<-E<gt>check($given_password)>.
+
+Not without some danger, so read on, you could also string compare it to the
+output of another C<password()>, as long as the salt is the same. Actually, if
+you are running on B<Linux/glibc> you can pass the first password as the salt
+to the second and it will get it right. Anyway, the danger:
 
 If the given string is already hashed it is assumed to be okay to use it as is.
-This means users can supply pre-hashed passwords to you.
+So if you are checking something from the outside world, C<-E<gt>check($it)>
+against the thing you can trust. You could also use C<crypt_password()>, which
+will definitely crypt its input.
+
+This means simpler code and users can supply pre-hashed passwords initially, but
+if you do it wrong a stolen hash could be used as a password, so buck up your ideas.
+
+If you aren't running B<Linux/glibc>, everything after the WARNING in the synopsis
+is dubious as. If you've got insight into how this module can work better on
+B<Darwin/FreeSec> I would love to hear from you.
 
 =head1 FUNCTIONS
 
@@ -192,6 +286,11 @@ This means users can supply pre-hashed passwords to you.
 =item password ( $password [, $salt [, $algorithm]] )
 
 Constructs a Crypt::Password object.
+
+=item crypt_password ( $password [, $salt [, $algorithm]] )
+
+Same as above but will definitely crypt $password, even if it looks crypted.
+See warning labels.
 
 =back
 
@@ -203,30 +302,27 @@ Constructs a Crypt::Password object.
 
 Checks the given password hashes the same as that this object represents.
 
-=item hash
+=item crypt
 
-Returns the hash.
+Returns the crypt string, same stringifying the object.
 
 =item salt
 
 Returns the salt.
 
-=item algorithm
-
-Returns the algorithm by name.
-
-=item algorithm_arg
-
-Returns the algorithm as it is represented in the Modular Crypt Formatted
-output of C<crypt(3)>.
-
 =back
 
 =head1 KNOWN ISSUES
 
-Cryptographic functionality depends greatly on your local glibc's B<crypt(3)>.
+Cryptographic functionality depends greatly on your local B<crypt(3)>.
 Old Linux may not support sha*, many other platforms only support md5, or that
-and Blowfish, etc.
+and Blowfish, etc. You are likely fine.
+
+On FreeSec's crypt, the crypted format is much different. Firstly, salt strings
+must be either two or eight characters long, in the latter case they will be
+prepended with an underscore for you. In the string you get back we also put the
+salt between two dollar signs, to make it slightly less ambiguous, less likely
+for C<password()> to assume something is crypted when it is not...
 
 =head1 SUPPORT, SOURCE
 
--- t/01-crypt-password.t
+++ t/01-crypt-password.t
@@ -2,7 +2,6 @@
 use strict;
 use warnings;
 use Test::More 'no_plan';
-use v5.10;
 
 use FindBin '$Bin';
 use lib "$Bin/../lib";
@@ -10,75 +9,163 @@ use_ok "Crypt::Password";
 
 sub mock { bless {@_}, "Crypt::Password" };
 
-{
+no warnings 'once', 'redefine';
+my $glib = $Crypt::Password::glib;
+diag "testing Crypt::Password (glib=".($glib ? "yes" : "no").")";
+diag "os is $^O";
+
+if ($glib) {
     diag "set algorithm";
     my $c = mock;
     is $c->algorithm, "sha256", "default algorithm";
     is $c->{algorithm}, "sha256", "default algorithm";
-    is $c->{algorithm_magic}, '$5$', "default algorithm magic";
+    is $c->{algorithm_id}, '5', "default algorithm id";
     
     is $c->algorithm("sha512"), "sha512", "set algorithm (sha512)";
     is $c->{algorithm}, "sha512", "set algorithm (sha512)";
-    is $c->{algorithm_magic}, '$6$', "set algorithm magic";
+    is $c->{algorithm_id}, '6', "set algorithm id";
     
     is $c->algorithm, "sha512", "get algorithm";
     
-    is $c->algorithm('$1$'), "md5", "set algorithm by magic (md5)";
+    is $c->algorithm('1'), "md5", "set algorithm by id (md5)";
     is $c->{algorithm}, "md5", "correct set algorithm (sha512)";
-    is $c->{algorithm_magic}, '$1$', "correct set algorithm magic";
+    is $c->{algorithm_id}, '1', "correct set algorithm id";
     
     is $c->algorithm, "md5", "get algorithm";
     
-    is $c->algorithm('$3a$'), undef, "set unknown magic";
-    is $c->{algorithm}, undef, "unknown magic";
-    is $c->{algorithm_magic}, '$3a$', "magic";
+    is $c->algorithm('3a'), undef, "set unknown id";
+    is $c->{algorithm}, undef, "unknown id";
+    is $c->{algorithm_id}, '3a', "id";
 }
 
-{
+if ($glib) {
     diag "generate salt";
     my $c = mock;
     my $salt_1 = $c->salt;
-    ok $salt_1 =~ /^\S{8}$/, "salt generated";
+    like $salt_1, qr/^\S{8}$/, "salt generated";
     $c = mock;
     my $salt_2 = $c->salt;
-    ok $salt_1 =~ /^\S{8}$/, "salt generated";
-    ok $salt_1 ne $salt_2, "generated salts are different";
+    like $salt_1, qr/^\S{8}$/, "salt generated";
+    isnt $salt_1, $salt_2, "generated salts are different";
     
     is $c->salt("4fatness"), "4fatness", "salt set, returned";
     is $c->{salt}, "4fatness", "salt set, returned";
 }
 
-{
+if ($glib) {
     diag "crypt some text";
     
     my $c = password("hello0");
-    ok $c =~ /^\$5\$(........)\$...........+$/, "crypted";
+    like $c, qr/^\$5\$(........)\$[a-zA-Z0-9\.\/]{43}$/, "crypted";
     
     my $c2 = password("hello0");
-    ok $c2 =~ /^\$5\$(........)\$...........+$/, "another crypted";
-    ok $c ne $c2, "generated different salts";
+    like $c2, qr/^\$5\$(........)\$[a-zA-Z0-9\.\/]{43}$/, "another crypted";
+    isnt $c, $c2, "generated different salts";
     ok $c->check("hello0"), "validates";
     ok !$c->check("hello1"), "invalidates";
+    
+    my $c3 = password("hello0", $c->salt);
+    is($c, $c3, "same salt");
+    ok($c3->check("hello0"), "yes indeed");
+}
+else {
+    my $c = password("hello0");
+    like $c, qr/^\$_\S{8}\$\S{11}$/, "crypted: looks good";
+    like password("hello0", "dg"), qr/^\$..\$\S{11}$/, "crypted: 2char supplied salt";
+
+    diag "various salt inputs";
+    # all invalid
+    for my $salt ("dgdb", "a", "123456", "1234567", "123456789") {
+        eval { password("hello0", $salt) };
+        like $@, qr/Bad salt input.+2 or 8 characters/, "wrong sized salt";
+        $@ = "";
+    }
+    for my $salt ("_a", "_bb") {
+        eval { password("hello0", "_a") };
+        like $@, qr/Bad salt input.+2-character salt cannot start with _/,
+            "can't start with _";
+        $@ = "";
+    }
+
+    # all valid
+    my $p;
+    eval { $p = password('a', 'bbbbbbbb') };
+    is $@, "", "salt=8 no error";
+    is $p, '$_bbbbbbbb$DJEHexiq9NI', "salt=8 crypt";
+    undef $@;
+
+    eval { $p = password('a', 'cc') };
+    is $@, "", "salt=2 no error";
+    is $p, '$cc$DFDkLhMbQ7wZ.', "salt=2 crypt";
+    undef $@;
+
+    diag "checks, comparisons";
+    ok($c->check("hello0"), "check the correct password");
+    ok(!$c->check("helow"), "check the wrong password");
+    isnt($c, password("hello0", "ga"), "compare a password - wrong salt");
+    isnt($c, password("hello0", "DADAdada"), "compare a password - wrong salt");
+    isnt($c, password("hello0", "etcetcet"), "compare a password - wrong salt");
+    is($c, password("hello0", $c->salt), "compare a password - correct salt");
+    isnt($c, password("hello1", $c->salt), "wrong password");
+    
+    isnt(password("007", "blahblah"), password("007", "BLAHblah"), "compare a password - wrong salt");
+    is(my $c2 = password("123", "12341234"), password("123", "12341234"), "compare a password - correct salt");
+    ok($c2->check("123"), "check the correct password");
+    ok(!$c2->check("12341234"), "check the wrong password");
+    ok(!$c2->check($c2), "can't just pass crypted stuff into check()");
+    ok(!$c2->check(password("123", $c2->salt)), "can't just pass crypted stuff into check()");
+
+    diag "remake some known crypts";
+    my @answers = map {[ split /\s+/, $_ ]} split /\n/, <<'ANSWERS';
+ambiente $_12345555$V4oENXvTMYk $gi$CZewZaJV4pk
+lampshade $_12345555$JacsOKd1xTo $gi$zi7R25ah3Zw
+guitar $_12345555$2yFp.wqJEF. $gi$4tl8fx6Anh.
+ANSWERS
+
+    for my $row (@answers) {
+        is(password($row->[0], "12345555"), $row->[1], "test $row->[0] salt=8");
+        is(password($row->[0], "gi"), $row->[2], "test $row->[0] salt=2");
+    }
+
+    diag "reinstating a crypt object";
+    my $c2_2 = password("$c2");
+    is($c2, $c2_2, "stringified and back");
+    is("$c2", "$c2_2", "stringified and back");
+    ok($c2_2->check("123"), "stringified and back, check correct");
+    ok(!$c2_2->check("23"), "stringified and back, check incorrect");
+    ok($c2->check("123"), "123 still good");
+    is($c2_2->salt, "12341234", "can extract the salt");
+    ok(!password("$c2")->check('$_12341234$123'), "can't just pass crypted stuff into check()");
 }
 
-{
+if ($glib) {
     diag "documented stuff";
     {
         my $hashed = password("password", "salt");
-        ok $hashed =~ /^\$5\$salt\$.{43}$/, "Default algorithm, supplied salt";
+        like $hashed, qr/^\$5\$salt\$.{43}$/, "Default algorithm, supplied salt";
     }
 
     {
         my $hashed = password("password", "", "md5");
-        ok $hashed =~ /^\$1\$\$.{22}$/, "md5, no salt";
+        like $hashed, qr/^\$1\$\$.{22}$/, "md5, no salt";
     }
 
     {
         my $hashed = password("password", undef, "sha512");
-        ok $hashed =~ /^\$6\$(.{8})\$.{86}$/, "sha512, invented salt";
+        like $hashed, qr/^\$6\$(.{8})\$.{86}$/, "sha512, invented salt";
+    }
+
+    {
+        my $password = '$5$%RK2BU%L$aFZd1/4Gpko/sJZ8Oh.ZHg9UvxCjkH1YYoLZI6tw7K8';
+        is $password, password($password), "password embodied by password()";
+        isnt $password, crypt_password($password), "password recrypted by crypt_password()";
+
+        # lately insane
+        ok password($password) eq password($password), "comparison test";
+        my $p1 = password($password);
+        my $p2 = password($password);
+        ok $p1 eq $p2, "comparison test";
     }
 }
-
-diag `man crypt`;
 
 1;
